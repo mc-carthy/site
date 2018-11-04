33501a69aa7094322199581169ed5f17e1e53157
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-11-04 13:53:48 +0000

    Add wildcard value to select filter

--- src/app/shared/filter-array.pipe.ts
+++ src/app/shared/filter-array.pipe.ts
@@ -7,7 +7,7 @@ import { PipeTransform } from '@angular/core';
 })
 export class FilterArrayPipe implements PipeTransform {
     transform(value: any, filterString: string, propName: string) {
-        if (value.length === 0 || filterString === '') {
+        if (value.length === 0 || filterString === '' || filterString === '*') {
             return value
         }
 
