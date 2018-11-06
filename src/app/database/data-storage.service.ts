ed047016e8267db5c954c5efb840b756365cff0e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-11-06 20:51:12 +0000

    Add safeguard against blogs having missing tag properties

--- src/app/database/data-storage.service.ts
+++ src/app/database/data-storage.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
 import { Constants } from '../shared/constants';
 import { Blog } from '../blog/blog.model';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class DataStorageService {
@@ -11,13 +12,33 @@ export class DataStorageService {
 
     }
 
+    // TODO: Enable the bmodified method below to ensure the response corresponds 
+    // to the blog model. This will need to be done when storing tags as an array. 
+    // If there are no tags, we will return nothing, as opposed to an empty array.
     getBlogs() {
-        this.http.get(Constants.getDbUrl() + 'blogs.json').subscribe(
-            (response: Response) => {
-                const blogs: Blog[] = response.json();
-                this.blogService.setBlogs(blogs);
-            }
-        );
+        this.http.get(Constants.getDbUrl() + 'blogs.json')
+        //     .pipe(map(
+        //         (response: Response) => {
+        //             const blogs: Blog[] = response.json();
+        //             for (let blog of blogs) {
+        //                 if (!blog.tags) {
+        //                     blog['tags'] = [];
+        //                 }
+        //             }
+        //             return blogs;
+        //         }
+        //     ))
+        //     .subscribe(
+        //         (blogs: Blog[]) => {
+        //             this.blogService.setBlogs(blogs);
+        //         }
+        //     );
+            .subscribe(
+                (response: Response) => {
+                    const blogs: Blog[] = response.json();
+                    this.blogService.setBlogs(blogs);
+                }
+            );
     }
 
     putBlogs() {
