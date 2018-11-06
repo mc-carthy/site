f8a59dd95b2ec44379afda145dda9717c32a9fc4
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-11-06 21:50:54 +0000

    Allow permanent deletion of blogs

--- src/app/blog/blog-entry/blog-entry.component.ts
+++ src/app/blog/blog-entry/blog-entry.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Params, ActivatedRoute, Router } from '@angular/router';
+import { Response } from '@angular/http';
+import { Subscription } from 'rxjs';
 import { Blog } from './../blog.model';
 import { BlogService } from './../blog.service';
+import { DataStorageService } from './../../database/data-storage.service';
 
 @Component({
     selector: 'app-blog-entry',
@@ -13,7 +16,12 @@ export class BlogEntryComponent implements OnInit {
     id: number;
     blog: Blog;
 
-    constructor(private route: ActivatedRoute, private router: Router, private blogService: BlogService) { }
+    constructor(
+        private route: ActivatedRoute, 
+        private router: Router, 
+        private blogService: BlogService,
+        private dataStorageService: DataStorageService
+    ) { }
 
     ngOnInit() {
         this.route.params.subscribe(
@@ -27,6 +35,11 @@ export class BlogEntryComponent implements OnInit {
     onDelete() {
         if (confirm('Are you sure you want to delete this blog entry?')) {
             this.blogService.deleteBlog(this.id);
+            this.dataStorageService.putBlogs().subscribe(
+                (response: Response) => {
+                    console.log(response);
+                }
+            );
             this.router.navigate(['/blogs']);
         }
     }
