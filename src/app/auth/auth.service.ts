ac3d5da40f7ff3728fb93ee9ef51c61aa68785a6
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-11-16 15:56:01 +0000

    Add redirect to home on successful login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,10 +1,13 @@
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
 
+@Injectable()
 export class AuthService {
 
     token: string;
 
-    constructor() { }
+    constructor(private router: Router) { }
 
     signUp(email: string, password: string) {
         firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -17,6 +20,8 @@ export class AuthService {
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(
                 response => {
+                    // TODO: Investigate whether we can go to the previous page instead of home
+                    this.router.navigate(['/']);
                     firebase.auth().currentUser.getIdToken()
                         .then(
                             (token: string) => this.token = token
